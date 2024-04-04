c26d228e58af77b412dcde612a62d540a060d2e6
Lung Andreea Selena <lungandreeaselena@gmail.com>
2024-04-04 21:36:47 +0300

    Shop GUI done

---
+++ HarvestHaven/Shop.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HarvestHaven
+{
+    /// <summary>
+    /// Interaction logic for Shop.xaml
+    /// </summary>
+    public partial class Shop : Window
+    {
+        public Shop()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sell_Carrot_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
