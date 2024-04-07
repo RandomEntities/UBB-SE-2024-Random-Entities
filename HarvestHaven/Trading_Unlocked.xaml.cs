3d5b575adbe38a1e8db426d5ddaefd1cf4e5257d
JurjV <jurjvictorftw@gmail.com>
2024-04-07 23:53:55 +0300

    Finished the GUI for Trading

--- HarvestHaven/Trading_Unlocked.xaml.cs
+++ HarvestHaven/Trading_Unlocked.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static HarvestHaven.Trading_Unlocked;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace HarvestHaven
 {
@@ -19,9 +22,23 @@ namespace HarvestHaven
     /// </summary>
     public partial class Trading_Unlocked : Window
     {
+
         public Trading_Unlocked()
         {
             InitializeComponent();
+
+            //Button testButton = new Button() { Content="Click me!"};
+            //Trades_List.Items.Add(testButton);
+        }
+
+        private void Give_Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Get_Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
