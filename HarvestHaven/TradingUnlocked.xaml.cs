1c863c8f239869793ed49c774c9395bfa6655f52
JurjV <jurjvictorftw@gmail.com>
2024-04-10 16:20:31 +0300

    some changes to the trades
    I created the trade panel for the "All trades" window so that we know how each trade will look and can use that panel to generate the trades,also changed the label and photos from one generated trade dynamically so that I know how to do it with each trade.

--- HarvestHaven/TradingUnlocked.xaml.cs
+++ HarvestHaven/TradingUnlocked.xaml.cs
@@ -28,6 +28,23 @@ namespace HarvestHaven
         {
             this.farmScreen = farmScreen;
             InitializeComponent();
+
+            TradingPanel tradingPanel = new();
+
+            Trades_List.Items.Add(tradingPanel);
+
+            tradingPanel.AcceptButton.Click += (sender, e) => AcceptButton_Click(sender, e, tradingPanel);
+
+
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e,TradingPanel tradingPanel)
+        {
+            tradingPanel.LabelGive.Content = "120";
+            tradingPanel.ImageGet.Source = new BitmapImage(new Uri("/Assets/Sprites/Items/tomato.png", UriKind.Relative));
+
+            tradingPanel.LabelGet.Content = "250";
+            tradingPanel.ImageGet.Source = new BitmapImage(new Uri("/Assets/Sprites/Items/cow.png", UriKind.Relative));
         }
 
         private void Back_Button_Click(object sender, RoutedEventArgs e)
@@ -41,12 +58,24 @@ namespace HarvestHaven
 
         private void Give_Button_Click(object sender, RoutedEventArgs e)
         {
-           
+
         }
 
         private void Get_Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Confirm_Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Confirm_Cancel_Button.Content.Equals("Confirm"))
+            {
+                this.Confirm_Cancel_Button.Content = "Cancel";
+            }
+            else
+            {
+                this.Confirm_Cancel_Button.Content = "Confirm";
+            }
         }
     }
 }
