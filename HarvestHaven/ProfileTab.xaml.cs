932f5e9c1f27a80f148c3743235784836497a1f9
Portoky <janosakosmarton@gmail.com>
2024-04-16 21:57:24 +0300

    Filled data with profile without conflcts

--- HarvestHaven/ProfileTab.xaml.cs
+++ HarvestHaven/ProfileTab.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using HarvestHaven.Entities;
+using HarvestHaven.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,25 +31,52 @@ namespace HarvestHaven
             SwitchToAchievements();
         }
 
-        private void SwitchToAchievements()
+        private async void SwitchToAchievements()
         {
             achievementList.Visibility = Visibility.Visible;
             leaderboardList.Visibility = Visibility.Hidden;
             commentList.Visibility = Visibility.Hidden;
+            try
+            {
+                List<Achievement> list = await AchievementService.GetAllAchievementsAsync();
+                DataContext = list;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
 
-        private void SwitchToLeaderboard()
+        private async void SwitchToLeaderboard()
         {
             achievementList.Visibility = Visibility.Hidden;
             leaderboardList.Visibility = Visibility.Visible;
             commentList.Visibility = Visibility.Hidden;
+            try
+            {
+                List<User> list = await UserService.GetAllUsersSortedByCoinsAsync();
+                DataContext = list;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
 
-        private void SwitchToComments()
+        private async void SwitchToComments()
         {
             achievementList.Visibility = Visibility.Hidden;
             leaderboardList.Visibility = Visibility.Hidden;
             commentList.Visibility = Visibility.Visible;
+            try
+            {
+                List<Comment> list = await UserService.GetMyComments();
+                DataContext = list;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
 
         private void achievementButton_Click(object sender, RoutedEventArgs e)
@@ -66,17 +96,9 @@ namespace HarvestHaven
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            //VisitedFarm visitedFarm = new VisitedFarm(Guid.Parse("ff3b57f1-8830-4bc2-ae8c-bee2dde4db6d"), this);
-            //
-            //visitedFarm.Top = this.Top;
-            //visitedFarm.Left = this.Left;
-            //
-            //visitedFarm.Show();
-            //this.Hide();
-
             farmScreen.Top = this.Top;
             farmScreen.Left = this.Left;
-            
+
             farmScreen.Show();
             this.Close();
         }
